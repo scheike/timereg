9975f70b7a68de98fb303506a4f757925e6e4f56
Klaus Holst <klaus@holst.it>
2012-05-12 10:12:50 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@264 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tests/workshop.R
+++ inst/tests/workshop.R
@@ -3,7 +3,7 @@
 ## installation, R (>=2.12.0)
 ###############################
 
-##install.packages("mets")
+install.packages("mets")
 
 ###############################
 ## Load simulated data
@@ -28,15 +28,16 @@ or1$score
 
 pcif <- predict(cifmod,X=1,resample.iid=0,uniform=0,se=0)
 
+png(filename="pcif.png")
 plot(pcif,multiple=1,se=0,uniform=0,ylim=c(0,0.15))
+dev.off()
 
 ###############################
 ## Correcting for country
 ###############################
 
+png(filename="pcifl.png")
 table(prt$country)
-
-par(mfrow=c(1,2))
 
 times <- seq(60,100,by=1)
 cifmodl <-comp.risk(Surv(time,status>0)~-1+factor(country)+cluster(id),data=prt,
@@ -45,6 +46,7 @@ cifmodl <-comp.risk(Surv(time,status>0)~-1+factor(country)+cluster(id),data=prt,
 pcifl <- predict(cifmodl,X=diag(4),se=0,uniform=0)
 plot(pcifl,multiple=1,se=0,uniform=0,col=1:4,ylim=c(0,0.2))
 legend("topleft",levels(prt$country),col=1:4,lty=1)
+dev.off()
 
 theta.des <- model.matrix(~-1+factor(zyg),data=prt) ## design for MZ/DZ status
 or.country <- or.cif(cifmodl,data=prt,cause1=2,cause2=2,theta.des=theta.des,
@@ -57,8 +59,13 @@ cifmodlr <-comp.risk(Surv(time,status>0)~+1+const(factor(country))+cluster(id),d
                     cens.model="aalen",cens.formula=~~factor(country))
 pciflr <- predict(cifmodlr,X=rep(1,4),Z=rbind(c(0,0,0),c(1,0,0),c(0,1,0),c(0,0,1)),se=0,uniform=0)
 
+png(filename="pcif2.png")
+par(mfrow=c(1,2))
+plot(pcifl,multiple=1,se=0,uniform=0,col=1:4,ylim=c(0,0.2))
+legend("topleft",levels(prt$country),col=1:4,lty=1)
 plot(pciflr,multiple=1,se=0,uniform=0,col=1:4,ylim=c(0,0.2))
 legend("topleft",levels(prt$country),col=1:4,lty=1)
+dev.off()
 
 or.countryr <- or.cif(cifmodlr,data=prt,cause1=2,cause2=2,theta.des=theta.des,
                      theta=c(2.8,6.9),score.method="fisher.scoring")
@@ -69,17 +76,21 @@ summary(or.countryr)
 ###############################
 
 ### ignoring country 
-p33=bicomprisk(Hist(time,status)~strata(zyg)+id(id),data=prt,cause=c(2,2),return.data=1,robust=1)
+p33 <- bicomprisk(Hist(time,status)~strata(zyg)+id(id),data=prt,cause=c(2,2),return.data=1,robust=1)
 
 p33dz <- p33$model$"DZ"$comp.risk
 p33mz <- p33$model$"MZ"$comp.risk
 
+png(filename="p33dz.png")
 plot(p33dz,ylim=c(0,0.1))
+dev.off()
 
+png(filename="pcaconc.png")
 par(new=TRUE)
 plot(p33mz,ylim=c(0,0.1),col=3)
 title(main="Concordance Prostate cancer")
 lines(pcif$time,pcif$P1^2,col=2)
+dev.off()
 
 ### test for genetic effect 
 test.conc(p33dz,p33mz);
@@ -94,6 +105,7 @@ library(cmprsk)
 ftime <- data33$time
 fstatus <- data33$status
 table(fstatus)
+
 group <- data33$zyg
 graytest <- cuminc(ftime,fstatus,group)
 graytest
@@ -103,12 +115,14 @@ zygeffect <- comp.risk(Surv(time,status==0)~const(zyg),
                   cens.model="aalen",model="logistic",conservative=1)
 summary(zygeffect)
 
+png(filename="casewise.png")
 case33mz <- conc2case(p33mz,pcif)
 case33dz <- conc2case(p33dz,pcif)
 
 plot(case33mz$casewise,se=0,col=2)
 par(new=TRUE)
 plot(case33dz$casewise,se=0)
+dev.off()
 
 ###############################
 ## Effect of zygosity correcting for country
@@ -142,14 +156,14 @@ summary(zygeffectll)
 ## Liability model, ignoring censoring
 ###############################
 
-
 (M <- with(prt, table(cancer,zyg)))
+
 coef(lm(cancer~-1+zyg,prt))
 
 ## Saturated model
-system.time(bpmz <- 
+bpmz <- 
     biprobit(cancer~1 + cluster(id), 
-             data=subset(prt,zyg=="MZ"), eqmarg=TRUE))
+             data=subset(prt,zyg=="MZ"), eqmarg=TRUE)
 
 logLik(bpmz) # Log-likelihood
 AIC(bpmz) # AIC
@@ -158,15 +172,18 @@ vcov(bpmz) # Asymptotic covariance
 summary(bpmz) # concordance, case-wise, tetrachoric correlations, ...
 
 bp0 <- biprobit(cancer~1 + cluster(id)+strata(zyg), data=prt)
+
 summary(bp0)
 
 ## Eq. marginals MZ/DZ
 bp1 <- bptwin(cancer~1,zyg="zyg",DZ="DZ",id="id",type="u",data=prt)
 summary(bp1) # Components (concordance,cor,...) can be extracted from returned list
+
 compare(bp0,bp1) # LRT
 
 ## Polygenic model
 args(bptwin)
+
 bp2 <- bptwin(cancer~1,zyg="zyg",DZ="DZ",id="id",type="ace",data=prt)
 summary(bp2)
 
@@ -174,6 +191,7 @@ summary(bp2)
 ## Liability model, IPCW
 ###############################
 
+png(filename="ipw.png")
 ## Probability weights based on Aalen's additive model 
 prtw <- ipw(Surv(time,status==0)~country, data=prt,
             cluster="id",weightname="w") 
@@ -181,36 +199,45 @@ plot(0,type="n",xlim=range(prtw$time),ylim=c(0,1),xlab="Age",ylab="Probability")
 count <- 0
 for (l in unique(prtw$country)) {
     count <- count+1
+    prtw <- prtw[order(prtw$time),]
     with(subset(prtw,country==l), 
-         points(time,w,col=count,lwd=2))
+         lines(time,w,col=count,lwd=2))
 }
 legend("topright",legend=unique(prtw$country),col=1:4,pch=1)
+dev.off()
 
-system.time(bpmzIPW <- 
+bpmzIPW <- 
               biprobit(cancer~1 + cluster(id), 
                        data=subset(prtw,zyg=="MZ"), 
-                       weight="w"))
+                       weight="w")
 (smz <- summary(bpmzIPW))
 
+png(filename="cif2.png")
 ## CIF
 plot(pcif,multiple=1,se=0,uniform=0,ylim=c(0,0.15))
 abline(h=smz$prob["Marginal",],lwd=c(2,1,1))
 ## Wrong estimates:
 abline(h=summary(bpmz)$prob["Marginal",],lwd=c(2,1,1),col="lightgray")
+dev.off()
 
+png(filename="conc2.png")
 ## Concordance
-plot(p33mz,ylim=c(0,0.05))
+plot(p33mz,ylim=c(0,0.1))
 abline(h=smz$prob["Concordance",],lwd=c(2,1,1))
 ## Wrong estimates:
 abline(h=summary(bpmz)$prob["Concordance",],lwd=c(2,1,1),col="lightgray")
+dev.off()
 
 bp3 <- bptwin(cancer~1,zyg="zyg",DZ="DZ",id="id",
               type="ace",data=prtw,weight="w")
 summary(bp3)
+
 bp4 <- bptwin(cancer~1,zyg="zyg",DZ="DZ",id="id",
               type="u",data=prtw,weight="w")
 summary(bp4)
+
 score(bp4) ## Check convergence
+
 bp5 <- bptwin(cancer~1,zyg="zyg",DZ="DZ",id="id",
               type="ade",data=prtw,weight="w")
 summary(bp5)
@@ -223,16 +250,13 @@ bp6 <- bptwin(cancer~country,zyg="zyg",DZ="DZ",id="id",
               type="ace",data=prtw,weight="w")
 summary(bp6)
 
-bp6 <- bptwin(cancer~country,zyg="zyg",DZ="DZ",id="id",
-              type="ace",data=prtw,weight="w")
-summary(bp6)
-
 bp7 <- bptwin(cancer~country,zyg="zyg",DZ="DZ",id="id",
               type="u",data=prtw,weight="w")
 summary(bp7)
 
 bp8 <- bptwin(cancer~strata(country),zyg="zyg",DZ="DZ",id="id",
               type="u",data=prtw,weight="w")
+
 summary(bp8)
 
 ## Wald test
@@ -244,8 +268,12 @@ compare(bp8,contrast=B)
 ###############################
 
 args(cumh)
+
 ch1 <- cumh(cancer~1,time="time",zyg="zyg",DZ="DZ",id="id",
             type="ace",data=prtw,weight="w")
+#+BEGIN_SRC R
 summary(ch1)
 
+png(filename="cumh.png")
 plot(ch1)
+dev.off()
