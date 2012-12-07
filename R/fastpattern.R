f043e306868681451b9d5f320747a14c43ded78f
Klaus Holst <klaus@holst.it>
2012-12-07 13:14:16 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@449 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/fastpattern.R
@@ -0,0 +1,16 @@
+##' Fast pattern
+##'
+##' @title Fast pattern
+##' @param x Matrix (binary) of patterns. Optionally if \code{y} is
+##' also passed as argument, then the pattern matrix is defined as the
+##' elements agreeing in the two matrices.
+##' @param y Optional matrix argument with same dimensions as
+##' \code{x} (see above)
+##' @param ... Optional additional arguments
+##' @author Klaus K. Holst
+##' @export
+fastpattern <- function(x,y,...) {
+  if (missing(y)) y <- NULL
+   .Call("FastPattern",x,y,DUP=FALSE)
+}
+ 
