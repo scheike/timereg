50f5ad57d1b41fb3fdc54e6e8915b2413860bf44
Thomas Scheike <ts@biostat.ku.dk>
2013-01-14 12:13:17 +0000

    ændrer Nit=100 i pc twostage som default
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@473 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/twostage.R
+++ R/twostage.R
@@ -517,7 +517,7 @@ return(out)
 
 ##' @export
 piecewise.twostage <- function(cut1,cut2,data=sys.parent(),timevar="time",status="status",id="id",covars=NULL,num=NULL,
-            score.method="optimize",Nit=60,detail=0,clusters=NULL,silent=1,weights=NULL,
+            score.method="optimize",Nit=100,detail=0,clusters=NULL,silent=1,weights=NULL,
             control=list(),theta=NULL,theta.des=NULL,var.link=1,iid=0,step=0.5,model="plackett",data.return=0)
 { ## {{{
 
