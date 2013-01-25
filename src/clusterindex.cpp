7882edeaa41ab2c07c93b19c5783dc58cc30b5dd
Thomas Scheike <ts@biostat.ku.dk>
2013-01-25 12:59:31 +0000

    clusterindex til ccp
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@486 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/clusterindex.cpp
+++ src/clusterindex.cpp
@@ -3,60 +3,74 @@
 #include <cmath>
 
 /* how many are there of the different clusters, similar to table(clusters) */ 
-RcppExport SEXP nclust(SEXP npers,SEXP clusters) {
-  try {
+RcppExport SEXP nclust(SEXP in,SEXP iclusters) {
+
+  uvec clusters = Rcpp::as<uvec>(iclusters); 
+  int  n = Rcpp::as<int>(in);
+  int uniqueclust=0; 
+
+  uvec nclust = zeros(n); 
 
   int i,maxclust=0;
-  for (i=0;i<*npers;i++){
-      if (nclust[clusters[i]]==0) uniqueclust[0]+=1; 
+  for (i=0;i<n;i++){
+      if (nclust[clusters[i]]==0) uniqueclust+=1; 
       nclust[clusters[i]]+=1; 
       if (nclust[clusters[i]]>maxclust) maxclust=nclust[clusters[i]]; 
   } 
 
-  mclust[0]=maxclust; 
-
-  }
-return(Rcpp::List::create( mclust, nclust)); 
+return(Rcpp::List::create(Rccp::Named("maxclust")=mclust,Rccp::Named("nclust")=nclust,
+			  Rccp::Named("uniqueclust")=uniqueclust)); 
 }
 
 /* organize indeces to different clusters in matrix  of size nclust x maxclust */ 
-RcppExport SEXP clusterindex(SEXP npers,SEXP clusters,SEXP nclust,SEXP mednum,SEXP num) 
+RcppExport SEXP clusterindex(SEXP in,SEXP iclusters,SEXP imacxlust,SEXP inclust,SEXP imednum,SEXP inum) 
 {
+
   int i;
+  int  n = Rcpp::as<int>(in);
+  uvec clusters = Rcpp::as<uvec>(iclusters); 
+  int maxclust = Rcpp::as<int>(imaxclust); 
+  int nclust = Rcpp::as<int>(inclust); 
+  uvec num = Rcpp::as<uvec>(inum); 
+  int  mednum = Rcpp::as<int>(imednum);
 
-  if (*mednum==0) {
-     for (i=0;i<*npers;i++){
-         idclust[(clustsize[clusters[i]])*(*nclust)+clusters[i]]=i; 
+  umat idclust = zeros(nclust,maxclust); 
+  uvec clustsize = zeros(nclust); 
+
+  if (mednum==0) {
+     for (i=0;i<n;i++){
+         idclust[(clustsize[clusters[i]])*(nclust)+clusters[i]]=i; 
          clustsize[clusters[i]]+=1; 
       } 
   } else {
-    for (i=0;i<*npers;i++){
-        idclust[num[i]*(*nclust)+clusters[i]]=i; 
+    for (i=0;i<n;i++){
+        idclust[num[i]*(nclust)+clusters[i]]=i; 
         clustsize[clusters[i]]+=1; 
      } 
   }
-return(Rcpp::List::create( idclust, clustsize)); 
+return(Rcpp::List::create(Rccp::Named("idclustmat")=idclust,Rccp::Named("clustsize")=clustsize)); 
 }
 
-RcppExport SEXP clusterindexdata(SEXP npers,SEXP clusters,SEXP nclust,SEXP mednum,SEXP num,SEXP data) 
-{
-
-  int i,j;
-  if (*mednum==0) {
-     for (i=0;i<*npers;i++){
-         idclust[(clustsize[clusters[i]])*(*nclust)+clusters[i]]=i; 
-     for (j=0;j<*p;j++) nydata[(clustsize[clusters[i]]*(*p)+j)*(*nclust)+clusters[i]]=data[(*npers)*j+i]; 
-         clustsize[clusters[i]]+=1; 
-      } 
-  } else {
-    for (i=0;i<*npers;i++){
-        idclust[num[i]*(*nclust)+clusters[i]]=i; 
-        for (j=0;j<*p;j++) nydata[(num[i]*(*p)+j)*(*nclust)+clusters[i]]=data[(*npers)*j+i]; 
-        clustsize[clusters[i]]+=1; 
-     } 
-  }
-
-return(Rcpp::List::create( idclust, clustsize,nydata)); 
-}
+//RcppExport SEXP clusterindexdata(SEXP npers,SEXP clusters,SEXP nclust,SEXP mednum,SEXP num,SEXP data) 
+//{ // {{{
+//
+//  int i,j;
+//  if (*mednum==0) {
+//     for (i=0;i<*npers;i++){
+//         idclust[(clustsize[clusters[i]])*(*nclust)+clusters[i]]=i; 
+//     for (j=0;j<*p;j++) nydata[(clustsize[clusters[i]]*(*p)+j)*(*nclust)+clusters[i]]=data[(*npers)*j+i]; 
+//         clustsize[clusters[i]]+=1; 
+//      } 
+//  } else {
+//    for (i=0;i<*npers;i++){
+//        idclust[num[i]*(*nclust)+clusters[i]]=i; 
+//        for (j=0;j<*p;j++) nydata[(num[i]*(*p)+j)*(*nclust)+clusters[i]]=data[(*npers)*j+i]; 
+//        clustsize[clusters[i]]+=1; 
+//     } 
+//  }
+//
+//return(Rcpp::List::create( idclust, clustsize,nydata)); 
+//} // }}}
+//
 
 
