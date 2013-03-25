18c1f06a07b451acb4f4357b4a48b548a9c16467
scheike <scheike@edab9b15-3c32-4f9a-a569-739a535b425d>
2013-03-25 08:03:30 +0000

    update svn
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/timereg@158 edab9b15-3c32-4f9a-a569-739a535b425d

--- pkg/MultiComp/R/First.r
+++
@@ -1,9 +0,0 @@
-.packageName <- "MultiComp"
-
-.First.lib <- function(lib, pkg) {
-  library.dynam("MultiComp", pkg, lib)
-}
-
-.Last.lib <- function(lib){
-  library.dynam.unload("MultiComp",lib)
-}
