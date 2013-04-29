094d65e423438b150440e669de573cb86e94e86e
scheike <scheike@edab9b15-3c32-4f9a-a569-739a535b425d>
2013-04-29 06:48:08 +0000

    krylow pls fra AGR
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/timereg@179 edab9b15-3c32-4f9a-a569-739a535b425d

--- pkg/timereg/R/krylow.pls.r
+++ pkg/timereg/R/krylow.pls.r
@@ -1,13 +1,29 @@
-krylow.pls<-function(D,d,dim)
-{
-R=d;  Sxxsxy=R;
-if (dim>=2)
-for (i in 2:dim)
-{
-Sxxsxy=D %*% Sxxsxy  ;
-R=cbind(R,Sxxsxy);
-}
-beta= R %*% solve(t(R) %*% D %*% R) %*% t(R) %*% d
-beta=  beta;
-return(list(beta=beta))
+###krylow.pls<-function(D,d,dim)
+###{
+###R=d;  Sxxsxy=R;
+###if (dim>=2)
+###for (i in 2:dim)
+###{
+###Sxxsxy=D %*% Sxxsxy  ;
+###R=cbind(R,Sxxsxy);
+###}
+###beta= R %*% solve(t(R) %*% D %*% R) %*% t(R) %*% d
+###beta=  beta;
+###return(list(beta=beta))
+###}
+
+krylow.pls <- function(D,d,K = 1) {
+	r <- d
+	p <- r
+	rsold <- drop(t(r) %*% r) 
+	x <- rep(0,length(d)) 
+	for(k in 1:K){
+		Ap <- D %*% p;
+		alpha <-drop(rsold / (t(p) %*% Ap)); x <- x + alpha * p;
+		r <- r - alpha * Ap;
+		rsnew <- drop(t(r) %*% r);
+		p <- r + rsnew / rsold * p;
+		rsold <-rsnew;
+	}
+	return(x) 
 }
