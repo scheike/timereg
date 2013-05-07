c46dafb9a1486e187593c1c00dd3021434d31d19
Thomas Scheike <ts@biostat.ku.dk>
2013-05-07 19:09:58 +0000

    prop-odds-test
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@647 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/prop-odds.R
@@ -0,0 +1,71 @@
+##' Aalen additive hazards model with (gamma) frailty
+##'
+##' Aalen frailty model
+##' @title Aalen frailty model
+##' @param time Time variable
+##' @param status Status variable (0,1)
+##' @param X Covariate design matrix
+##' @param id cluster variable
+##' @param theta list of thetas (returns score evaluated here), or
+##' starting point for optimization (defaults to magic number 0.1)
+##' @param ... Additional arguments to lower level functions
+##' @return Parameter estimates
+##' @author Klaus K. Holst
+##' @export
+##' @examples
+##' n <- 5e3
+##' theta <- 0.3
+##' K <- 2
+##' Z <- rep(rgamma(n/K,1/theta,1/theta),each=K)
+##' id <- rep(seq(n/K),each=K)
+##' beta0 <- 1.5; beta <- 0##-0.5 #-.15 
+##' Ai <- function(u) (u/Z)/(beta0+beta*x)
+##' x <- rbinom(n,1,0.5)
+##' uu <- -log(runif(n)) ##rexp(n,1)
+##' dd <- data.frame(time=Ai(uu), x=x, status=1, id=id, Z=Z)
+##' cens <- runif(n,10,20)
+##' dd$status <- (dd$time<=cens)*1
+##' dd$time <- apply(cbind(dd$time,cens),1,min)
+##' dd <- dd[order(dd$time),] ## order after event/censoring tim
+##' f <- ~1##+x
+##' X <- model.matrix(f,dd) ## design matrix for non-parametric terms
+##' system.time(out<-aalen(update(f,Surv(time,status)~.),dd,n.sim=0,robust=0))
+##' dix <- which(dd$status==1)
+##' t1 <- system.time(bb <- .Call("Bhat",as.integer(dd$status),X,theta,as.integer(dd$id),NULL,NULL,-1,package="mets"))
+##' spec <- 1
+##' ##plot(out,spec=spec)
+##' plot(dd$time[dix],bb$B2[,spec],col="red",type="s",ylim=c(0,max(dd$time)*c(beta0,beta)[spec]))
+##' abline(a=0,b=c(beta0,beta)[spec])
+##'
+##' 
+##' \dontrun{thetas <- seq(0.1,2,length.out=10)
+##' Us <- unlist(aalenfrailty(dd$time,dd$status,X,dd$id,as.list(thetas)))
+##' plot(thetas,Us,type="l",ylim=c(-.5,1)); abline(h=0,lty=2); abline(v=theta,lty=2)
+##' op <- aalenfrailty(dd$time,dd$status,X,dd$id)
+##' op}
+propOdds <- function(time,status,X,beta,
+                         ...) {  
+  id <- 1:length(time)
+  theta <- 1
+  dix <- which(status==1)
+  fB <- fast.approx(time[dix],time)
+  cc <- cluster.index(id)
+  ncluster <- length(cc$clusters)
+  U <- function(beta,indiv=FALSE) {
+    B <- .Call("pBhat",as.integer(status),X,beta,as.integer(cc$clusters),cc$idclust,as.integer(cc$cluster.size))$B
+    Ba <- B[fB$pos+1,,drop=FALSE]
+    Hij <- as.vector(X*Ba)
+    res <- .Call("Uhat",as.integer(status),Hij,theta,cc$idclust,as.integer(cc$cluster.size))
+    if (!indiv) res <- mean(res)
+    res
+  }
+  if (missing(theta)) theta <- 0.1
+  if (is.list(theta)) return(lapply(theta,function(x) U(x,...)))
+
+  op <- nlminb(theta,function(x) U(x)^2)
+  uu <- U(op$par,TRUE)
+  du <- grad(U,op$par)
+  return(list(theta=op$par, sd=(mean(uu^2)/du^2/ncluster)^0.5))
+}
+                  
+  
---
+++ src/prop-odd.cpp
@@ -0,0 +1,133 @@
+#include <RcppArmadillo.h>
+#include <Rmath.h>
+#include <cmath>
+
+using namespace arma;
+
+RcppExport SEXP pBhat(SEXP ds,
+		     SEXP Xs, 
+		     SEXP beta, 		     
+		     SEXP id, 
+		     SEXP ididx,
+		     SEXP idsize) {
+  try {
+
+    uvec           event = Rcpp::as<uvec>(ds); 
+    mat                X = Rcpp::as<mat>(Xs);
+    mat               Xc = zeros<mat>(X.n_cols,X.n_cols);
+    vec         betahat  = Rcpp::as<double>(beta);
+    unsigned  stop,start = X.n_rows;
+    uvec        eventpos = find(event==1);
+    mat               dB = zeros(eventpos.n_elem,X.n_cols);
+    uvec         cluster = Rcpp::as<uvec>(id);
+    double thetahat=1; 
+
+    uvec clustersize, clustpos;
+    umat clusterindex;
+    bool doclust = (Rf_isNull)(idsize);
+    if (!doclust)  {
+      clustersize        = Rcpp::as<uvec>(idsize);
+      clusterindex       = Rcpp::as<umat>(ididx);
+    }
+
+    // Obtain usual estimates of increments, dB, of the
+    // cumulative time-varying effects in Aalens Additive Model
+    for (unsigned ij=0; ij<eventpos.n_elem; ij++) {
+      unsigned rij = eventpos.n_rows-ij-1;
+      stop  = start-1;
+      start = eventpos(rij);
+      mat Xij = X(span(start,stop), span::all);
+      Xc = Xc + Xij.st()*Xij;
+      mat U, V; vec s; 
+      svd(U,s,V,Xc);
+      mat Xci = U*diagmat(1/s)*V.st();
+      dB.row(rij) = trans(Xci*trans(X.row(start)));
+    }
+
+
+    vec       Hij(X.n_rows); // Vector to hold cumulative hazard; updated as t increases
+    Hij.fill(0);
+    mat        B2 = zeros(eventpos.n_elem,X.n_cols); // Conditional cumulative hazard 
+    for (unsigned k=0; k<eventpos.n_elem; k++) { // Iterate over events
+      unsigned ij = eventpos(k);
+      unsigned i = cluster(ij);  // cluster
+      if (doclust) {
+         clustpos = find(cluster==i); // position of within cluster observations
+      } else {
+        unsigned csize = clustersize(i);
+        clustpos  = conv_to<uvec>::from(clusterindex.submat(i,0,i,csize-1));
+      }
+      uvec posL = find(clustpos<ij); // earlier events/censoring within cluster
+      uvec posR = find(clustpos>=ij); // later/current events within cluster
+      unsigned Ni = 0; // Number of events in cluster before current event,time t-
+      double Hi = 0 ; // Sum of cum.haz. within cluster up to time t-
+      if (posL.n_elem>0) {
+	Ni = sum(event.elem(clustpos.elem(posL)));
+	Hi = sum(Hij.elem(clustpos.elem(posL)));
+      }
+      uvec pos;
+      if (posR.n_elem>0 && k>0) {
+	pos = clustpos.elem(posR);
+	mat Xi = X.rows(pos);
+	Hij.elem(pos) = Xi*trans(B2.row(k-1));
+	Hi += sum(Hij.elem(pos));
+      }
+      double psi = (1/thetahat+Ni)/(1/thetahat+fmax(0,Hi));
+      B2.row(k) = dB.row(k)/psi;
+      if (k>0) { B2.row(k) += B2.row(k-1); }
+    }
+    return(Rcpp::List::create(Rcpp::Named("dB")=dB,
+			      Rcpp::Named("B2")=B2
+			      ));
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {  
+    ::Rf_error( "c++ exception (unknown reason)" ); 
+  }
+  return R_NilValue; // -Wall
+}
+
+
+RcppExport SEXP Uhat(SEXP ds, SEXP H, SEXP theta, SEXP id, SEXP idsize) {
+  try {
+    uvec                event = Rcpp::as<uvec>(ds);
+    vec                   Hij = Rcpp::as<vec>(H);
+    double           thetahat = Rcpp::as<double>(theta);
+    umat              cluster = Rcpp::as<umat>(id);
+    uvec clustersize, ucluster, clustpos;
+    unsigned nclust;
+    bool doclust = (Rf_isNull)(idsize);
+    //bool doclust = (cluster.n_cols==1);
+    if (doclust)  {
+      ucluster              = unique(cluster);
+      nclust                = ucluster.n_elem;
+    } else {
+      clustersize           = Rcpp::as<uvec>(idsize);
+      nclust = cluster.n_rows;
+    }
+    vec res(nclust);
+    for (unsigned i=0; i<nclust; i++) {
+      if (doclust) {
+        unsigned ic = ucluster(i);  // cluster
+        clustpos = find(cluster==ic); // position of within cluster observations
+      } else {
+        unsigned csize = clustersize(i);
+        clustpos  = conv_to<uvec>::from(cluster.submat(i,0,i,csize-1));
+	//cluster(span(i,i),span(0,csize-1));
+      }
+      double Ni = sum(event.elem(clustpos));
+      double Hi = sum(Hij.elem(clustpos));
+      double thetaH = thetahat*Hi+1;
+      double R = (log(thetaH)/thetahat + (Ni-Hi)/(thetaH));
+      for (unsigned h=0; h<Ni; h++) R -= 1/(1+thetahat*h);
+      res(i) = R/thetahat;
+    }
+    return(Rcpp::wrap(res));
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+  } catch(...) {  
+    ::Rf_error( "c++ exception (unknown reason)" ); 
+  }
+  return R_NilValue; // -Wall
+}
+
