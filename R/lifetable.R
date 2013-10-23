489a12404d442f95e494580c2517bb0c36f40cae
Klaus Holst <klaus@holst.it>
2013-10-23 13:17:46 +0000

    lifetable function
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@734 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/lifetable.R
@@ -0,0 +1,69 @@
+##' Creat simple life table 
+##'
+##' 
+##' @title Life table
+##' @param time Time (or matrix with columns time,status or entry,time,status)
+##' @param status status (event=TRUE,censoring=FALSE)
+##' @param entry Entry time
+##' @param strata Strata
+##' @param breaks Time intervals
+##' @author Klaus K. Holst
+##' @examples
+##' library(timereg)
+##' data(TRACE)
+##' with(TRACE, lifetable(cbind(time,status==9),breaks=c(0.2,0.5),confint=TRUE))
+##' 
+##' d <- with(TRACE, lifetable(time,status==9,strata=list(sex=sex,vf=vf),breaks=c(0.2,0.5)))
+##' summary(glm(events ~ offset(log(atrisk))+interval+sex+vf,data=d,poisson))
+##' 
+##' @export
+lifetable <- function(time,status,entry,strata=list(),breaks=c(),confint=FALSE) {
+    if (missing(entry)) entry <- rep(0,NROW(time))
+    if ((is.matrix(time) || is.data.frame(time)) && ncol(time)>1) {
+        if (ncol(time)==3) {
+            status <- time[,3]
+            entry <- time[,1]
+            time <- time[,2]
+        } else {
+            status <- time[,2]
+            time <- time[,1]
+            entry <- rep(0,length(time))
+        }
+    }
+    if (length(strata)>0) {
+        a <- by(cbind(entry,time,status), strata,
+                FUN=lifetable, breaks=breaks, confint=confint)
+        res <- cbind(Reduce("rbind",a),
+                     do.call("expand.grid",attributes(a)$dimnames))
+        return(res)
+    }    
+    if (length(breaks)>0) breaks <- sort(unique(breaks))
+    en <- matrix(unlist(lapply(c(-Inf, breaks),function(x) pmax(x,entry))),
+                  ncol=length(breaks)+1)
+    ex <- matrix(unlist(lapply(c(breaks, Inf),function(x) pmin(x,time))),
+                  ncol=length(breaks)+1)    
+    dur <- ex-en
+    endur <- rbind(c(breaks,Inf))%x%cbind(rep(1,nrow(en)))-en
+    dur[dur<0] <- NA
+    enter <- colSums(!is.na(dur))
+    atrisk <- colSums(dur,na.rm=TRUE)
+    eventcens <- dur<endur
+    eventcens <- rbind(apply(dur<endur,2,function(x) x*(status+1)))
+    lost <- colSums(eventcens==1,na.rm=TRUE)
+    events <- colSums(eventcens==2,na.rm=TRUE)
+    res <- data.frame(enter=enter,
+                      atrisk=atrisk,
+                      lost=lost,
+                      events=events,
+                      interval=as.factor(c(breaks,Inf)),
+                      rate=events/atrisk)
+    if (confint) {
+        f <- events ~ offset(log(atrisk))
+        if (length(breaks)>0) f <- update(f,.~.+factor(interval)-1)
+        g <- glm(f,data=res,poisson)
+        ci <- rbind(exp(stats::confint(g)))
+        res[,"2.5%"] <- ci[,1]
+        res[,"97.5%"] <- ci[,2]
+    }
+    res
+}
