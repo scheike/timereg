2c9d75509d47509bb0dc7c96a5468151665081b1
Klaus Holst <klaus@holst.it>
2013-10-27 19:08:50 +0000

    data help updated. dermal ridges data added
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@752 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/mets-packages.R
+++ R/mets-packages.R
@@ -21,6 +21,7 @@ NULL
 ##' @name np
 ##' @docType data
 ##' @keywords data
+##' @source Simulated data
 NULL
 
 ##' Migraine data
@@ -30,12 +31,45 @@ NULL
 ##' @keywords data
 NULL
 
+##' Dermal ridges data (families)
+##'
+##' Data on dermal ridge counts in left and right hand in (nuclear) families
+##' @name dermalridges
+##' @docType data
+##' @keywords data
+##' @format Data on 50 families with ridge counts in left and right
+##' hand for moter, father and each child. Family id in 'family' and
+##' gender and child number in 'sex' and 'child'.
+##' @source Sarah B. Holt (1952). Genetics of dermal ridges: bilateral
+##' asymmetry in finger ridge-counts.  Annals of Eugenics 17 (1),
+##' pp.211--231. DOI: 10.1111/j.1469-1809.1952.tb02513.x
+##' @examples
+##' data(dermalridges)
+##' fast.reshape(mets::dermalridges,id="family",varying=c("child.left","child.right","sex"))
+NULL
 
-##' Menarche data set (sim)
+##' Dermal ridges data (monozygotic twins)
+##'
+##' Data on dermal ridge counts in left and right hand in (nuclear) families
+##' @name dermalridgesMZ
+##' @docType data
+##' @keywords data
+##' @format Data on dermal ridge counts (left and right hand) in 18
+##' monozygotic twin pairs.
+##' @source Sarah B. Holt (1952). Genetics of dermal ridges: bilateral
+##' asymmetry in finger ridge-counts.  Annals of Eugenics 17 (1),
+##' pp.211--231. DOI: 10.1111/j.1469-1809.1952.tb02513.x
+##' @examples
+##' data(dermalridgesMZ)
+##' fast.reshape(dermalridgesMZ,id="id",varying=c("left","right"))
+NULL
+
+##' Menarche data set
 ##'
 ##' @name mena
 ##' @docType data
 ##' @keywords data
+##' @source Simulated data
 NULL
 
 ##' Multivariate Cumulative Incidence Function example data set
@@ -43,6 +77,7 @@ NULL
 ##' @name multcif
 ##' @docType data
 ##' @keywords data
+##' @source Simulated data
 NULL
 
 ##' Stutter data set
@@ -59,11 +94,12 @@ NULL
 ##' @keywords data
 NULL
 
-##' Prostate data set (sim)
+##' Prostate data set
 ##'
 ##' @name prt
 ##' @docType data
 ##' @keywords data
+##' @source Simulated data
 NULL
 
 ##' For internal use
