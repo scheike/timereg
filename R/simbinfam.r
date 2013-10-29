eb41b68c337a2aac31799abd2c0b517303b1057a
Thomas Scheike <ts@biostat.ku.dk>
2013-10-29 12:31:23 +0000

    binfam
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@770 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/simbinfam.r
+++ R/simbinfam.r
@@ -1,18 +1,23 @@
 
 ##' @export 
-sim.bin.fam <- function(n,beta=0.0,vargam1=1,vargam2=1) { ## {{{ 
-xm <- rbinom(n,1,0.5); xf <- rbinom(n,1,0.5); 
-xb1 <- rbinom(n,1,0.5); xb2 <- rbinom(n,1,0.5); 
+sim.bin.fam <- function(n,beta=0.0,rhopp=0.1,rhomb=0.7,rhofb=0.1,rhobb=0.7) { ## {{{ 
+xc <- runif(n)*0.5
+xm <- rbinom(n,1,0.5+xc); 
+xf <- rbinom(n,1,0.5+xc); 
+xb1 <- rbinom(n,1,0.3+xc); 
+xb2 <- rbinom(n,1,0.3+xc); 
 ###
-lam1 <- vargam1; lam2 <- vargam2
-zf <- rgamma(n,shape=1/lam1)*lam1; zb <- rgamma(n,shape=1/lam2)*lam2; 
-pm <- exp(0.5+xm*beta+zf)
+rn <- matrix(rnorm(n*4),n,4)
+corm <- matrix( c(1,rhopp,rhomb,rhomb, rhopp,1,rhofb,rhofb, rhomb,rhofb,1,rhobb, rhomb,rhofb,rhobb,1),4,4)
+rnn <- t( corm %*% t(rn))
+zm <- exp(rnn[,1]); zf <- exp(rnn[,2]); zb1 <- exp(rnn[,3]); zb2 <- exp(rnn[,4]); 
+pm <- exp(0.5+xm*beta+zm)
 pf <- exp(0.5+xf*beta+zf)
 pf <- pf/(1+pf)
 pm <- pm/(1+pm)
-pb1 <- exp(0.5+xb1*beta+zf+zb)
+pb1 <- exp(0.5+xb1*beta+zb1)
 pb1 <- pb1/(1+pb1)
-pb2 <- exp(0.5+xb2*beta+zf+zb)
+pb2 <- exp(0.5+xb2*beta+zb2)
 pb2 <- pb2/(1+pb2)
 ym <- rbinom(n,1,pm)
 yf <- rbinom(n,1,pf)
