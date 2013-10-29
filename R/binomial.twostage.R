3c89ab8ac5c349134cd4062673d95a13b645948d
Thomas Scheike <ts@biostat.ku.dk>
2013-10-29 13:29:31 +0000

    var der allerede
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@771 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/binomial.twostage.R
+++ R/binomial.twostage.R
@@ -37,8 +37,8 @@
 ##' 
 ##' twinstut$binstut <- (twinstut$stutter=="yes")*1
 ##' out <- easy.binomial.twostage(stutter~factor(sex)+age,data=twinstut,
-##'                               response="binstut",id="tvparnr",
-##' 			      theta.formula=~-1+factor(zyg1),
+##'                           response="binstut",id="tvparnr",
+##' 	             	      theta.formula=~-1+factor(zyg1),
 ##'                               score.method="fisher.scoring")
 ##' summary(out)
 ##' 
@@ -483,6 +483,37 @@ p00 <- 1- p10-p01-p11
 y1 <- rbinom(n,1,p1)
 y2 <- (y1==1)*rbinom(n,1,p11/p1)+(y1==0)*rbinom(n,1,p01/(1-p1))
 list(x1=x1,x2=x2,y1=y1,y2=y2,id=1:n)
+} ## }}} 
+
+##' @export 
+simBinFam <- function(n,beta=0.0,rhopp=0.1,rhomb=0.7,rhofb=0.1,rhobb=0.7) { ## {{{ 
+xc <- runif(n)*0.5
+xm <- rbinom(n,1,0.5+xc); 
+xf <- rbinom(n,1,0.5+xc); 
+xb1 <- rbinom(n,1,0.3+xc); 
+xb2 <- rbinom(n,1,0.3+xc); 
+###
+rn <- matrix(rnorm(n*4),n,4)
+corm <- matrix( c(1,rhopp,rhomb,rhomb, rhopp,1,rhofb,rhofb, rhomb,rhofb,1,rhobb, rhomb,rhofb,rhobb,1),4,4)
+rnn <- t( corm %*% t(rn))
+zm <- exp(rnn[,1]); zf <- exp(rnn[,2]); zb1 <- exp(rnn[,3]); zb2 <- exp(rnn[,4]); 
+pm <- exp(0.5+xm*beta+zm)
+pf <- exp(0.5+xf*beta+zf)
+pf <- pf/(1+pf)
+pm <- pm/(1+pm)
+pb1 <- exp(0.5+xb1*beta+zb1)
+pb1 <- pb1/(1+pb1)
+pb2 <- exp(0.5+xb2*beta+zb2)
+pb2 <- pb2/(1+pb2)
+ym <- rbinom(n,1,pm)
+yf <- rbinom(n,1,pf)
+yb1 <- rbinom(n,1,pb1)
+yb2 <- rbinom(n,1,pb2)
+#
+agem <- 20+runif(n)*10
+ageb1 <- 5+runif(n)*10
+data.frame(agem=agem,agef=agem+3+rnorm(n)*2,
+	   ageb1=ageb1,ageb2=ageb1+1+runif(n)*3,xm=xm,xf=xf,xb1=xb1,xb2=xb2,ym=ym,yf=yf,yb1=yb1,yb2=yb2,id=1:n)
 } ## }}} 
 
 ##' @export
