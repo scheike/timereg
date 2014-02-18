485b713ab6fd9ca904ee708b999b96246c06a54f
scheike <scheike@edab9b15-3c32-4f9a-a569-739a535b425d>
2014-02-18 14:08:59 +0000

    sindex local igen
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/timereg@285 edab9b15-3c32-4f9a-a569-739a535b425d

--- pkg/timereg/R/Cpred.r
+++ pkg/timereg/R/Cpred.r
@@ -13,7 +13,7 @@ pred<-rep(0,nval);
 ###as.integer(Tminus),PACKAGE="timereg")
 
 ### sindex from prodlim
-xval.order <- sindex(cumtimes,xval, comp=comp,strict=strict)
+xval.order <- sindex.prodlim(cumtimes,xval, comp=comp,strict=strict)
 pred.begin <-  xval.order
 pred.begin[xval.order==0] <- 1
 predcum <- as.matrix(designX[pred.begin,-1])
@@ -23,22 +23,22 @@ if (order==FALSE) return(cbind(xval,predcum)) else return(list(xval.order=xval.o
 }
 
 ## sindex fra prodlim, thanks to Thomas Gerds
-###sindex.prodlim <- function (jump.times, eval.times, comp = "smaller", strict = FALSE)
-###{   
-###    stopifnot(is.numeric(jump.times))
-###    stopifnot(is.numeric(eval.times))
-###        N <- length(jump.times)
-###    if (comp == "greater") {
-###       N - sindex.prodlim(jump.times = jump.times, eval.times = eval.times, comp = "smaller", strict = !strict)
-###    }
-###    else {
-###           neval <- length(eval.times)
-###           if (!(neval > 0 && N > 0))
-###	                stop("missing data")
-###           new.order <- order(eval.times)
-###           ind <- .C("sindex", index = integer(neval), as.double(sort(jump.times)),
-###                  as.double(eval.times[new.order]), as.integer(N),
-###                  as.integer(neval), as.integer(strict), DUP = FALSE, PACKAGE = "timereg")$index
-###		   ind[order(new.order)]
-###    }
-###}
+sindex.prodlim <- function (jump.times, eval.times, comp = "smaller", strict = FALSE)
+{   
+    stopifnot(is.numeric(jump.times))
+    stopifnot(is.numeric(eval.times))
+        N <- length(jump.times)
+    if (comp == "greater") {
+       N - sindex.prodlim(jump.times = jump.times, eval.times = eval.times, comp = "smaller", strict = !strict)
+    }
+    else {
+           neval <- length(eval.times)
+           if (!(neval > 0 && N > 0))
+	                stop("missing data")
+           new.order <- order(eval.times)
+           ind <- .C("sindex", index = integer(neval), as.double(sort(jump.times)),
+                  as.double(eval.times[new.order]), as.integer(N),
+                  as.integer(neval), as.integer(strict), PACKAGE = "timereg")$index
+		   ind[order(new.order)]
+    }
+}
