6868575461ab9c4c946a8a8e5250bff40352eeea
Klaus Holst <klaus@holst.it>
2014-05-06 12:37:09 +0000

    rename and updated ipw->ipcw. Now with parametric models via phreg.par
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lava/pkg/mets@1078 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/ipw.R
+++ R/ipw.R
@@ -14,7 +14,7 @@
 ##' @author Klaus K. Holst
 ##' @examples
 ##' data(prt)
-##' prtw <- ipw(Surv(time,status==0)~country, data=prt[sample(nrow(prt),5000),],
+##' prtw <- ipcw(Surv(time,status==0)~country, data=prt[sample(nrow(prt),5000),],
 ##'             cluster="id",weightname="w")
 ##' plot(0,type="n",xlim=range(prtw$time),ylim=c(0,1),xlab="Age",ylab="Probability")
 ##' count <- 0
@@ -26,83 +26,71 @@
 ##' }
 ##' legend("topright",legend=unique(prtw$country),col=1:4,pch=-1,lty=1)
 ##' @export
-ipw <- function(formula,data,cluster,
-                samecens=FALSE, obsonly=TRUE,weightname="w",
-                cens.model="aalen", pairs=FALSE, ...) {
+ipcw <- function(formula,data,cluster,
+                 samecens=FALSE,obsonly=TRUE,weightname="w",
+                 iid=TRUE,
+                 cens.model="aalen", pairs=FALSE, ...) {
 
-  timevar <- as.character(terms(formula)[[2]][[2]])
-  otimes <- data[,timevar]
-  utimes <- sort(unique(otimes))
-  delta <- min(diff(c(0,utimes)))/2
-  ## We want prediction just before event
-  ##  if (length(attributes(terms(formula))$term.labels)) {    
-  ##    fit <- cph(formula,data=data,surv=TRUE,x=TRUE,y=TRUE)
-  ##    pr <- survest(fit,what="parallel",newdata=data,
-  ##                    times=otimes-delta)
-  ##  } else { ## cph does not work without covariates.. Kaplan-Meier:  
-  ##  }
-  ##  fit <- survfit(formula,data=data);
-  ##  sfit <- summary(fit)
-  ##  stratas <- fit$strata
-  ##  if (is.null(stratas)) {    
-  ##  Gfit <- cbind(fit$time,fit$surv)
-  ##  pr <- fastapprox(Gfit[,1],otimes-delta,Gfit[,2])[[1]]
-  ##    Gfit2<-rbind(c(0,1),Gfit); 
-  ##    pr<-Cpred(Gfit2,otimes)[,2];
-  ## } else {
-  ##   for (s in stratas) {      
-  ##   }
-  ## }
-  
-  XZ <- model.matrix(formula,data)
-  cens.args <- c(list(formula,n.sim=0,robust=0,data=data),list(...))
-  ud.cens <- do.call(cens.model,cens.args)
-  Gcx<-Cpred(ud.cens$cum,otimes)[,-1];
-  Gcx<-exp(-apply(Gcx*XZ,1,sum))
-  Gcx[Gcx>1]<-1; Gcx[Gcx<0]<-0
-  pr <- Gcx  
-  
-  noncens <- with(data,!eval(terms(formula)[[2]][[3]]))
-  data[,weightname] <- pr  
-  if (samecens & !missing(cluster)) {
-    message("Minimum weights...")
-    myord <- order(data[,cluster])
-    data <- data[myord,,drop=FALSE]
-    id <-  table(data[,cluster])
-    if (pairs) {
-      gem <- data[,cluster]%in%(names(id)[id==2])
-      id <- id[id==2]
-      data <- data[gem,]
+
+    XZ <- model.matrix(formula,data)
+    cens.args <- c(list(formula,n.sim=0,robust=0,data=data),list(...))
+    if (tolower(cens.model)%in%c("weibull","phreg.par","phreg.weibull")) {
+        ud.cens <- do.call(mets:::phreg.par,cens.args)
+        pr <- predict(ud.cens)
+        noncens <- which(!ud.cens$status)        
+    } else {
+        m <- match.call(expand.dots = TRUE)[1:3]
+        Terms <- terms(formula, data = data, env=parent.frame())
+        m$formula <- Terms
+        m[[1]] <- as.name("model.frame")
+        M <- eval(m,parent.frame())
+        censtime <- model.extract(M, "response")
+        status <- censtime[,2]
+        otimes <- censtime[,1]
+        noncens <- !status
+        ud.cens <- do.call(cens.model,cens.args)
+        Gcx<-Cpred(ud.cens$cum,otimes)[,-1];
+        Gcx<-exp(-apply(Gcx*XZ,1,sum))
+        Gcx[Gcx>1]<-1; Gcx[Gcx<0]<-0
+        pr <- Gcx
     }
-    d0 <- subset(data,select=c(cluster,weightname))
-    noncens <- with(data,!eval(terms(formula)[[2]][[3]]))
-    d0[,"observed."] <- noncens
-    timevar <- paste("_",cluster,weightname,sep="")
-    d0[,timevar] <- unlist(lapply(id,seq))
-    Wide <- reshape(d0,direction="wide",timevar=timevar,idvar=cluster)
-    W <- apply(Wide[,paste(weightname,1:2,sep=".")],1,
-               function(x) min(x,na.rm=TRUE))
-    Wmarg <- d0[,weightname]
-    data[,weightname] <- 1/Wmarg
-    Wmin <- rep(W,id)
-    ##    d0[,weightname] <- 0
-
-##################################################
-############################################################
-##################################################
-
-    ##      Wcomb <- (Wmin-Wmarg)/(Wmarg*Wmin)      
-    obs1only <- rep(with(Wide, observed..1 & (is.na(observed..2) | !observed..2)),id)
-    obs2only <- rep(with(Wide, observed..2 & (is.na(observed..1) | !observed..1)),id)
-    obsOne <- which(na.omit(obs1only|obs2only))
-    obsBoth <- rep(with(Wide, !is.na(observed..1) & !is.na(observed..2) & observed..2 & observed..1),id)
-
-    data[obsBoth,weightname] <-
-      ifelse(noncens[obsBoth],1/Wmin[obsBoth],0)    
-    data[obsOne,weightname] <-
-      ifelse(noncens[obsOne],1/Wmarg[obsOne],0)
-  } 
-  if (obsonly)
-    data <- data[noncens,,drop=FALSE]
-  return(data)    
+    data[,weightname] <- pr
+    
+    if (samecens & !missing(cluster)) {        
+        message("Minimum weights...")
+        myord <- order(data[,cluster])
+        data <- data[myord,,drop=FALSE]
+        id <-  table(data[,cluster])
+        if (pairs) {
+            gem <- data[,cluster]%in%(names(id)[id==2])
+            id <- id[id==2]
+            data <- data[gem,]
+        }
+        d0 <- subset(data,select=c(cluster,weightname))
+        noncens <- with(data,!eval(terms(formula)[[2]][[3]]))
+        d0[,"observed."] <- noncens
+        timevar <- paste("_",cluster,weightname,sep="")
+        d0[,timevar] <- unlist(lapply(id,seq))
+        Wide <- reshape(d0,direction="wide",timevar=timevar,idvar=cluster)
+        W <- apply(Wide[,paste(weightname,1:2,sep=".")],1,
+                   function(x) min(x,na.rm=TRUE))
+        Wmarg <- d0[,weightname]
+        data[,weightname] <- 1/Wmarg
+        Wmin <- rep(W,id)
+        
+        obs1only <- rep(with(Wide, observed..1 & (is.na(observed..2) | !observed..2)),id)
+        obs2only <- rep(with(Wide, observed..2 & (is.na(observed..1) | !observed..1)),id)
+        obsOne <- which(na.omit(obs1only|obs2only))
+        obsBoth <- rep(with(Wide, !is.na(observed..1) & !is.na(observed..2) & observed..2 & observed..1),id)
+        
+        data[obsBoth,weightname] <-
+            ifelse(noncens[obsBoth],1/Wmin[obsBoth],0)    
+        data[obsOne,weightname] <-
+            ifelse(noncens[obsOne],1/Wmarg[obsOne],0)
+    }
+    
+    if (obsonly)
+        data <- data[noncens,,drop=FALSE]
+    
+    return(data)    
 }
