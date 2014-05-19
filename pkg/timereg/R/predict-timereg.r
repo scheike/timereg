bf9c3302fa0fc620508006cdefea00f9d654cfa8
scheike <scheike@edab9b15-3c32-4f9a-a569-739a535b425d>
2014-05-19 09:54:32 +0000

    prediction update times argument
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/timereg@299 edab9b15-3c32-4f9a-a569-739a535b425d

--- pkg/timereg/R/predict-timereg.r
+++ pkg/timereg/R/predict-timereg.r
@@ -62,7 +62,7 @@ predict.cox.aalen <- function(object,...) predict.timereg(object,...)
 predict.aalen <- function(object,...) predict.timereg(object,...)
 predict.comprisk <- function(object,...) predict.timereg(object,...)
 
-predict.timereg <-function(object,newdata=NULL,X=NULL,
+predict.timereg <-function(object,newdata=NULL,X=NULL,times=NULL,
                            Z=NULL,n.sim=500, uniform=TRUE,
                            se=TRUE,alpha=0.05,resample.iid=0,...)
 {
@@ -83,7 +83,6 @@ predict.timereg <-function(object,newdata=NULL,X=NULL,
     stop("resample processes necessary for these computations, set resample.iid=1");
   }
   if (is.null(object$gamma)==TRUE) { semi<-FALSE } else { semi<-TRUE }
-  if (se==FALSE) uniform <- FALSE
 
   ## {{{ extracts design based on the different specifications
   ## cox.aalen uses prop(...), while aalen and comp.risk use const(...)
@@ -104,7 +103,9 @@ predict.timereg <-function(object,newdata=NULL,X=NULL,
     ## Then extract the time-varying effects
     ###    time.coef <- data.frame(object$cum)
     time.coef <- as.matrix(object$cum)
-    if (modelType=="cox.aalen" && (!is.null(object$time.sim.resolution))) time.coef <- Cpred(object$cum,object$time.sim.resolution)
+    if (!is.null(times)) time.coef <- Cpred(time.coef,times)
+    ### SE based on iid decomposition so uses time-resolution for cox.aalen model 
+    if (modelType=="cox.aalen" && (!is.null(object$time.sim.resolution)) && (se==TRUE)) time.coef <- Cpred(object$cum,object$time.sim.resolution)
     ntime <- nrow(time.coef)
     fittime <- time.coef[,1,drop=TRUE]
     ntimevars <- ncol(time.coef)-2
