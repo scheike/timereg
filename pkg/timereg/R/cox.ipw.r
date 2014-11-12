20b03b906c8da7303624d156c961f5c1173dfdc5
scheike <scheike@edab9b15-3c32-4f9a-a569-739a535b425d>
2014-11-12 14:25:18 +0000

    ties nu med i cox.ipw
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/timereg@341 edab9b15-3c32-4f9a-a569-739a535b425d

--- pkg/timereg/R/cox.ipw.r
+++ pkg/timereg/R/cox.ipw.r
@@ -1,4 +1,4 @@
-cox.ipw <- function(survformula,glmformula,d=sys.parent(),max.clust=NULL,ipw.se=FALSE)
+cox.ipw <- function(survformula,glmformula,d=sys.parent(),max.clust=NULL,ipw.se=FALSE,tie.seed=100)
 { ## {{{ 
   ggl <- glm(glmformula,family='binomial',data=d)
   mat <-  model.matrix(glmformula,data=d);
@@ -7,6 +7,22 @@ cox.ipw <- function(survformula,glmformula,d=sys.parent(),max.clust=NULL,ipw.se=
 
 ###  d1 <- d[,survcovs]
 ###  dcc <- na.omit(d)
+  ## {{{ checking and breaking ties
+  ties <- FALSE
+  survtimes <- all.vars(update(survformula,.~0))
+  if (length(survtimes)==2) {itime <- 1; time2 <- d[,survtimes[1]]; status <- d[,survtimes[2]]; }
+  if (length(survtimes)==3) {itime <- 2;  time2 <- d[,survtimes[2]]; status <- d[,survtimes[3]]; }
+  jtimes <- time2[status==1]; 
+  dupli <- duplicated(jtimes)
+  if (sum(dupli)>0) {
+	  set.seed(tie.seed)
+	  jtimes[dupli] <- jtimes[dupli]+runif(sum(dupli))*0.01
+	  time2[status==1] <- jtimes
+	  d[,survtimes[itime]] <- time2
+	  ties <- TRUE
+  }
+  ## }}}
+
   dcc <- d[ggl$y==1,]
   ppp <- dcc$ppp
   udca <- cox.aalen(survformula,data=dcc,weights=1/ppp,n.sim=0,max.clust=max.clust)  
@@ -43,11 +59,11 @@ iidfull[ggl$y==1,] <- coxiid - alphaiid[ggl$y==1,]
 ###
 var2 <- t(iidfull) %*% iidfull
 se <- cbind(diag(var2)^.5); colnames(se) <- "se"
-} else { var2 <- NULL; se <- NULL} ## }}} 
+} else { iidfull <- NULL; var2 <- NULL; se <- NULL} ## }}} 
 
 se.naive=coef(udca)[,3,drop=FALSE]; colnames(se.naive) <- "se.naive"
 
-res <- list(iid=iidfull,coef=udca$gamma,var=var2,se=se,se.naive=se.naive)
+res <- list(iid=iidfull,coef=udca$gamma,var=var2,se=se,se.naive=se.naive,ties=list(ties=ties,time2=time2))
 return(res)
 } ## }}} 
 
