84d00ec765cdc6686eedd47e6bd842adf119ae96
Thomas Scheike <ts@biostat.ku.dk>
2015-05-15 17:43:01 +0200

    update example ipw2

--- R/ipw.R
+++ R/ipw.R
@@ -181,35 +181,35 @@ ipw <- function(formula,data,cluster,
 ##' @param ... Additional arguments to censoring model 
 ##' @author Thomas Scheike 
 ##' @examples
-##' data(prt)
-##' m <- nrow(prt)
-##' prt$entry <- rep(rbinom(m/2,1,0.5)*runif(m/2)*100,each=2) 
-##' prtleft <- prt[prt$time>prt$entry,]
-##' 
-##' prtw <- ipw2(prtleft,times=80,
-##'             cluster="id",time="time",cause="status",strata="zyg",
-##'             entrytime="entry")
-##' 
-##' times <- seq(50,90,by=10)
-##' c1 <- comp.risk(Event(time,cause)~-1+factor(country)+
-##' 		cluster(id),data=prt,cause=1,
-##' 		times=times,max.clust=NULL,n.sim=0)
+##'
+##' d <- simnordic.random(5000,delayed=TRUE,ptrunc=0.7,
+##'       cordz=0.5,cormz=2,lam0=0.3,country=FALSE)
+##' d$strata <- as.numeric(d$country)+(d$zyg=="MZ")*4
+##' times <- 50:100
+##' ###cc1 <- comp.risk(Event(time,cause)~-1+factor(country)+
+##' c1 <- comp.risk(Event(time,cause)~1+
+##' 		cluster(id),data=d,cause=1,
+##' 		model="fg",times=times,max.clust=NULL,n.sim=0)
 ##' mm=model.matrix(~-1+zyg,data=d)
-##' out1<-random.cif(c1,data=d,cause1=1,cause2=1,same.cens=TRUE,
-##' 	  theta.des=mm)
+##' out1<-random.cif(c1,data=d,cause1=1,cause2=1,same.cens=TRUE,theta.des=mm)
 ##' summary(out1)
-##'
-##' c2 <- comp.risk(Event(time,cause)~-1+factor(country)+
-##'             cluster(id),
-##'		data=prtw,cause=1,model="fg",
-##'      	weights=prtw$indi.weights,
-##'             cens.weights=rep(1,nrow(prtw)),
+##' pc1 <- predict(c1,X=1,se=0)
+##' plot(pc1)
+##' 
+##' dl <- d[!d$truncated,]
+##' dl <- ipw2(dl,cluster="id",same.cens=TRUE,time="time",entrytime="entry",cause="cause",
+##'            strata="strata",prec.factor=100)
+##' cl <- comp.risk(Event(time,cause)~+1+
+##' 		cluster(id),
+##' 		data=dl,cause=1,model="fg",
+##' 		weights=dl$indi.weights,cens.weights=rep(1,nrow(dl)),
 ##'             times=times,max.clust=NULL,n.sim=0)
-##' mm=model.matrix(~-1+factor(zyg),data=prtw)
-##' out2<-random.cif(cl,data=prtw,cause1=1,cause2=1,
-##'   theta.des=mm,weights=prtw$weights,censoring.weights=rep(1,nrow(prtw)))
+##' pcl <- predict(cl,X=1,se=0)
+##' lines(pcl$time,pcl$P1,col=2)
+##' mm=model.matrix(~-1+factor(zyg),data=dl)
+##' out2<-random.cif(cl,data=dl,cause1=1,cause2=1,theta.des=mm,
+##'                  weights=dl$weights,censoring.weights=rep(1,nrow(dl)))
 ##' summary(out2)
-##'
 ##' @export
 ipw2 <- function(data,times=NULL,entrytime=NULL,time="time",cause="cause",
      same.cens=FALSE,cluster=NULL,pairs=FALSE,
