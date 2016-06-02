0f0689c35bb0038706eabf6133174f84c1dc92b0
Thomas Scheike <ts@biostat.ku.dk>
2016-06-02 12:42:57 +0200

    event.split update

--- R/event.split.r
+++ R/event.split.r
@@ -1,73 +1,63 @@
-
 event.split <- function(data,
 			time="time",status="status",cuts="cuts",
-			name.start="start",id=NULL,
 			name.id="id",
-			entry=NULL,cens.code=0,num="num")
+			name.start="start", 
+			cens.code=0, 
+			order.id=TRUE
+			)
 {
 ## {{{ 
     n <- nrow(data)
     new.time <- data[,time]
     new.status <- data[,status]
-    new.cuts <- data[,cuts]
-    if (!is.null(entry)) {
-      new.start <- data[,entry]
+
+    if (is.numeric(cuts)) {
+	    cutname <- paste("cut",cuts,sep=".")
+            data[,cutname] <- cuts
+    } else cutname <- cuts
+    new.cuts <- data[,cutname]
+
+    if (is.numeric(name.start)) {
+	    start0 <- name.start
+	    name.start <- paste("start",name.start,sep=".")
+            data[,name.start] <- start0
+    }  
+
+    if ((name.start %in% names(data))) {
+      new.start <- data[,name.start]
     } else new.start <- rep(0,n)
-    idl <- 1:n
+
     if (any(new.start>= new.time)) cat("any(new.start>= new.time) is TRUE\n"); 
 
-   if (name.id %in% names(data)) 
-	   name.id <-paste(id,".split",sep="")
+    if ((name.id %in% names(data))) idl <- data[,name.id] else {
+	    idl <- 1:n
+	    data[,name.id] <- idl 
+    }
 
-###    splits <- (new.cuts<new.time & new.start<new.cuts)
-###    print(sum(splits))
     splits <- which(new.cuts<new.time & new.start<new.cuts)
-    idl <- c(idl,idl[splits])
-    new.time <- c(new.time,new.time[splits])
-    new.start <- c(new.start,new.cuts[splits])
-    new.status <- c(new.status,new.status[splits])
-    new.ccc <- c(new.cuts,new.cuts[splits])
-    ###
-    new.time[splits] <- new.cuts[splits]
-    new.status[splits] <- cens.code
 
-    new.num <- c(rep(0,n),rep(1,length(splits)))
-    new.num <- (new.start>=new.ccc)*1 
-    data <- data[idl,]
-    data[,time] <- new.time
-    data[,status] <- new.status
-    data[,name.start] <- new.start
-    data[,name.id] <- idl
-    data[,num] <- new.num
-### data[,"ccc"] <- new.ccc
+    if (length(splits)) {
+	    rows  <- c(1:n,splits)
+	    new.time <-   c(new.time,new.time[splits])
+	    new.start <-  c(new.start,new.cuts[splits])
+	    new.status <- c(new.status,new.status[splits])
+	    new.ccc <-    c(new.cuts,new.cuts[splits])
+	    idl <- c(idl,idl[splits])
+	    new.time[splits] <- new.cuts[splits]
+	    new.status[splits] <- cens.code
+	    data <- data[rows,]
+	    data[,time] <- new.time
+	    data[,status] <- new.status
+	    data[,name.start] <- new.start
+	    data[,name.id] <- idl
+###    if (num %in% names(data))
+###        data[,num] <- data[,num] + new.num else data[,num] <- new.num
 
-    if (is.null(id)) data <- data[order(idl),] else data <- data[order(data[,id],new.start),]
+    }
+
+    if (order.id) data <- data[order(idl,new.start),] 
 
     return(data)
     ## }}} 
 } 
-
-###d <- data.frame(event=round(5*runif(5),2),
-###		start=round(5*runif(5),2),
-###		time=1:5,
-###		status=rbinom(5,1,0.5),
-###		x=1:5)
-###d$fcut <- 2
-###d
-###event.split(d,cuts="event",num="nume")
-###event.split(d,cuts="fcut",num="numf")
-###
-###### successive splitting 
-###de <- event.split(d,cuts="event",num="nume")
-###de
-###def <- event.split(de,cuts="fcut",entry="start",id="id",num="numf")
-######
-###### successive splitting 
-###df <- event.split(d,cuts="fcut",num="numf")
-###df
-###dfe <- event.split(df,cuts="event",entry="start",id="id",num="nume")
-###dfe <- dfe[,names(def)]
-###dfe
-###dfe==def
-
 
