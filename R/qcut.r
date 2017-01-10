edde45d6b5e47ca09ce98822b8ece83e41aef71a
Thomas Scheike <ts@biostat.ku.dk>
2017-01-10 14:10:39 +0100

    updates

--- R/qcut.r
+++ R/qcut.r
@@ -3,9 +3,9 @@ qcut <- function(x,cuts=4,breaks=NULL,...)
 {# {{{
 	if (is.null(breaks)) {
 		   probs <- seq(0,1,length.out=cuts+1)
-	   bb <- quantile(x,probs,...)
+	   bb <- quantile(x,probs)
 	} else bb <- breaks
-	gx<- cut(x,breaks=bb,include.lowest=TRUE)
+	gx<- cut(x,breaks=bb,include.lowest=TRUE,...)
 	return(gx)
 }# }}}
 
