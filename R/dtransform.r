440175a2bef3a44e6a3ffb05249080c3e9850476
Thomas Scheike <ts@biostat.ku.dk>
2017-02-11 16:40:16 +0100

    starting dtransform

---
+++ R/dtransform.r
@@ -0,0 +1,17 @@
+
+dtransform <- function(data,...,subset)
+{# {{{
+    if (is.vector(data)) data <- data.frame(data)
+    subs <- substitute(subset)
+    if (!base::missing(subs)) {
+        expr <- suppressWarnings(inherits(try(subset,silent=TRUE),"try-error"))
+        if (expr) data <- data[which(eval(subs,envir=data)),,drop=FALSE]
+        else data[subset,,drop=FALSE]
+    }
+
+    datanew <- transform(data,...)
+return(datanew)
+}# }}}
+
+###xx <- dtransform(mena,ll=log(agemena)+twinnum,zyg=="DZ")
+###xx <- dtransform(mena,ll=log(agemena)+twinnum,I(agemena)<15)
