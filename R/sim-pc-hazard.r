62ca21ae540bc9b765170646e7c2188f98f7da0c
Thomas Scheike <ts@biostat.ku.dk>
2017-07-13 13:04:37 +0200

    delayed entry for simulation of cumulative incidence

--- R/sim-pc-hazard.r
+++ R/sim-pc-hazard.r
@@ -73,7 +73,8 @@ pc.hazard <- function(cumhazard,rr,entry=NULL,cum.hazard=TRUE,cause=1)
   } else cumh <- cumhazard[,2] 
    n <- length(rr)
    ttt <- rexp(n)/rr
-   if (cumh[1]>0) warning("Safest to start with cumulative hazard 0 to avoid problems\n"); 
+   if (cumh[1]>0) 
+	   warning("Safest to start with cumulative hazard 0 to avoid problems\n"); 
    ###
    if (!is.null(entry)) {
 	   if (length(entry)==1) entry <- rep(entry,n) else entry <- entry
@@ -442,7 +443,7 @@ if (!is.list(coxs)) stop("Cox models in list form\n");
 #' cumhaz <- cif$cum
 #' 
 #' ## 1000 logistic without covariates with baseline from model fit  
-#' sim1 <- invsubdist(cumhaz,1000,type="logistic")
+#' sim1 <- simsubdist(cumhaz,1000,type="logistic")
 #' ###
 #' cifs <- comp.risk(Event(time,status)~+1,data=sim1,cause=1,model="logistic2")
 #' ###
@@ -452,7 +453,7 @@ if (!is.list(coxs)) stop("Cox models in list form\n");
 #' ## 1000 logistic with covariates with baseline from model fit  
 #' x <- rbinom(1000,1,0.5)
 #' rr <- exp(x*0.3)
-#' sim1 <- invsubdist(cumhaz,rr,type="logistic")
+#' sim1 <- simsubdist(cumhaz,rr,type="logistic")
 #' sim1$x <- x
 #'
 #' cifs <- comp.risk(Event(time,status)~+const(x),data=sim1,cause=1,model="logistic2")
@@ -473,8 +474,8 @@ if (!is.list(coxs)) stop("Cox models in list form\n");
 #' n=10000
 #' tt=seq(0,3,by=.01)
 #' tt=seq(0,3,by=.01)
-#' t1 <- invF1(cbind(tt,F1p(tt)),n)
-#' t2 <- invF1(cbind(tt,F1p(tt,lam0=0.1)),n)
+#' t1 <- invF1(cbind(tt,F1p(tt)),runif(n))
+#' t2 <- invF1(cbind(tt,F1p(tt,lam0=0.1)),runif(n))
 #' rt <- rbinom(n,1,(F1p(3)+F1p(3,lam0=0.1)))
 #' rb <- rbinom(n,1,F1p(3)/(F1p(3)+F1p(3,lam0=0.1)))
 #' cause=ifelse(rb==1,1,2)
@@ -501,9 +502,9 @@ if (!is.list(coxs)) stop("Cox models in list form\n");
 #' #lines(tt,F1p(tt),col=2)
 #' #plot(pp,cause="2")
 #' #lines(tt,F1p(tt,lam0=0.1),col=2)
-#' @aliases invsubdist invF1
+#' @aliases simsubdist invF1
 #' @export
-invsubdist <- function(cumhazard,rr,entry=NULL,type="cloglog",startcum=c(0,0),...)
+simsubdist <- function(cumhazard,rr,entry=NULL,type="cloglog",startcum=c(0,0),...)
 {# {{{
   ## Fine-Gray model cloglog F1= 1-exp(-cum(t)*rr)
   ## logistic                F1= cum(t)*rr/(1+cum(t)*rr)
@@ -545,26 +546,184 @@ invsubdist <- function(cumhazard,rr,entry=NULL,type="cloglog",startcum=c(0,0),..
    return(dt)
 }# }}}
 
-#' @aliases invF1
-#' @export
-invF1 <- function(F1,n,...)
+#' Finds inverse of piecwise linear sub-distribution 
+#' 
+#' Finds inverse of piecwise linear sub-distribution to be used for simulation of subdistributions
+#' 
+#' @param F1 matrix with x, and F1(x) 
+#' @param u points for which to compute inverse
+#' @param entry possible delayed entry points
+#' @param cond 1 indcates that we draw given that this subdistribution is used, so scales mass to 1 to get distribution function.
+#' @param ptrunc possible trunction weigth for delayed entry, if NULL then uses ptrunc=1-F1(entry)
+#' @author Thomas Scheike
+#' @keywords survival
+#' @examples
+#' F1 <- cbind(c(0,5,8,10),c(0,0.1,0.3,0.9))
+#' plot(F1,type="l")
+#' u <- runif(100)
+#' Fiu <- invF1(F1,u,cond=0)
+#' points(Fiu$time,u,pch="x")
+#' 
+#' F1cond <- F1
+#' F1cond[,2] <- F1cond[,2]/0.9
+#' plot(F1cond,type="l")
+#' u <- runif(100)
+#' Ficond <- invF1(F1cond,u,cond=0)
+#' points(Ficond$time,u,pch="-")
+#' Fiu <- invF1(F1,u,cond=1)
+#' points(Fiu$time,u,pch="x")
+#' 
+#' entry <- 4
+#' ###
+#' F1entry <- subdist(F1,entry)[,2]
+#' ptrunc <- 1-F1entry
+#' ###
+#' F1entry5 <- F1
+#' F1entry5[,1] <- F1entry5[,1]-entry
+#' F1entry5[,2] <- (F1entry5[,2]-F1entry)/ptrunc
+#' pos <- F1entry5[,1]>=0
+#' F1entry5 <- rbind(c(0,0),F1entry5[pos,])
+#' ###
+#' plot(F1entry5,ylim=c(0,1),type="l")
+#' u <- runif(100)
+#' Fiu <- invF1(F1entry5,u,cond=0)
+#' points(Fiu$time,u,pch="-")
+#' ###
+#' Fiu2 <- invF1(F1,u,cond=0,entry=entry)
+#' points(Fiu2$time-entry,u,pch="x")
+#' sum(Fiu2$time-entry-Fiu$time)
+#' 
+#' F1ce <- F1entry5
+#' F1ce[,2] <- F1ce[,2]/tail(F1entry5[,2],1)
+#' plot(F1ce,type="l")
+#' u <- runif(100)
+#' Fi1ce <- invF1(F1ce,u,cond=0)
+#' points(Fi1ce$time,u,pch="-")
+#' Fice <- invF1(F1,u,cond=1,entry=entry)
+#' points(Fice$time-entry,u,pch="x")
+#' sum(Fice$time-entry-Fi1ce$time)
+#' 
+#' 
+#' ## simulation of distribution with delayed entry starting at 3
+#' par(mfrow=c(1,1))
+#' F1 <- cbind(c(0,5,8,10),c(0,0.5,0.6,0.9))
+#' F1
+#' plot(F1,ylim=c(0,1),type="l")
+#' 
+#' n <- 100000
+#' entry <- c(rep(3,10000),runif(n)*7+3)
+#' ###entry <- rep(3,n)
+#' u <- runif(n+10000)
+#' ###
+#' Fiu <- invF1(F1,u,cond=0,entry=entry)
+#' ###
+#' # library(prodlim)
+#' # pp <- prodlim(Hist(time,status,entry=entry)~+1,data=Fiu)
+#' # plot(pp,xlim=c(3,10))
+#' ###
+#' entry <- 3
+#' ###
+#' F1entry <- subdist(F1,entry)[,2]
+#' ptrunc <- 1-F1entry
+#' ###
+#' F1entry5 <- F1
+#' F1entry5[,1] <- F1entry5[,1]-entry
+#' F1entry5[,2] <- (F1entry5[,2]-F1entry)/ptrunc
+#' pos <- F1entry5[,1]>=0
+#' F1entry5 <- rbind(c(0,0),F1entry5[pos,])
+#' #
+#' # lines(entry+F1entry5[,1],1-F1entry5[,2],col=2)
+#' 
+#' ##############################################################
+#' ## Simulations of two cumulative incidence functions with truncation 
+#' ##############################################################
+#' par(mfrow=c(1,1))
+#' F1 <- cbind(c(0,5,8,10),c(0,0.5,0.6,0.9)*0.3)
+#' F2 <- cbind(c(0,5,8,10),c(0,0.5,0.6,0.9)*0.5)
+#' plot(F1,ylim=c(0,1),type="l")
+#' lines(F2,col=2)
+#' entry1 <- 3
+#' ###
+#' F1entry <- subdist(F1,entry1)[,2]
+#' F2entry <- subdist(F2,entry1)[,2]
+#' ptrunc <- 1-F1entry-F2entry
+#' ###
+#' F1e <- F1
+#' F1e[,1] <- F1e[,1]-entry1
+#' F1e[,2] <- (F1e[,2]-F1entry)/ptrunc
+#' pos <- F1e[,1]>=0
+#' F1e <- rbind(c(0,0),F1e[pos,])
+#' F2e <- F2
+#' F2e[,1] <- F2e[,1]-entry1
+#' F2e[,2] <- (F2e[,2]-F2entry)/ptrunc
+#' pos <- F2e[,1]>=0
+#' F2e <- rbind(c(0,0),F2e[pos,])
+#' #
+#' # truncated identifiable version 
+#' lines(entry1+F1e[,1],F1e[,2],col=1)
+#' lines(entry1+F2e[,1],F2e[,2],col=2)
+#' 
+#' n <- 10000
+#' entry <- c(rep(entry1,10000),runif(n)*(10-entry1)+entry1)
+#' u <- runif(n+10000)
+#' ###
+#' F1entry <- subdist(F1,entry)[,2]
+#' F2entry <- subdist(F2,entry)[,2]
+#' ptrunc <- 1-( F1entry+F2entry)
+#' Fiu1 <- invF1(F1,u,cond=1,entry=entry,ptrunc=ptrunc)
+#' Fiu2 <- invF1(F1,u,cond=1,entry=entry,ptrunc=ptrunc)
+#' ###
+#' ptot <- (tail(F1[,2],1)+tail(F2[,2],1)-F1entry-F2entry)/(ptrunc)
+#' rt <- rbinom(n+10000,1,ptot)
+#' p1 <- ((tail(F1[,2],1)-F1entry)/ptrunc)
+#' p2 <- ((tail(F2[,2],1)-F2entry)/ptrunc)
+#' rb <- rbinom(n+10000,1,p1/ptot)
+#' cause=ifelse(rb==1,1,2)
+#' time=ifelse(cause==1,Fiu1$time,Fiu2$time)
+#' cause <- rt*cause
+#' time[cause==0] <- 10
+#' ### simulated data, now checking that things are working 
+#' # pp <- prodlim(Hist(time,cause,entry=entry)~+1)
+#' # plot(pp,xlim=c(entry1,10),cause=1)
+#' # plot(pp,xlim=c(entry1,10),cause=2,add=TRUE)
+#' ###
+#' # lines(entry1+F1e[,1],F1e[,2],col=2)
+#' # lines(entry1+F2e[,1],F2e[,2],col=2)
+#' 
+#' #' @aliases invF1
+#' #' @export
+invF1 <- function(F1,u,entry=NULL,cond=1,ptrunc=NULL)
 {# {{{
   x <- breaks <- F1[,1]
   y <- F1t <- F1[,2] 
   mm <- tail(breaks,1)
   F1m <- tail(F1t,1)
-  y <-  F1t/F1m
-  ttte <- runif(n) 
-  ri <- sindex.prodlim(y,ttte,strict=FALSE)
-  rrr <- (ttte-y[ri])/(y[ri+1]-y[ri])
+  n <- length(u)
+  if (!is.null(entry)) {
+      ###  if (length(entry)==1) entry <- rep(entry,n) else entry <- entry
+      ##   cumhazard at entry 
+      F1entry <- subdist(F1,entry)[,2]; 
+  } 
+  if (!is.null(entry)) 
+	  if (is.null(ptrunc)) ptrunc <- 1- F1entry
+  if (cond==0 & !is.null(entry))  u<-ptrunc*u+F1entry
+  if (cond==1 & !is.null(entry))  u<-u*(F1m-F1entry)+F1entry 
+###  if (cond==0 & is.null(entry))   u<-u
+  if (cond==1 & is.null(entry))   u<-u*F1m
+  y <- F1t
+  ri <- sindex.prodlim(y,u,strict=FALSE)
+  rrr <- (u-y[ri])/(y[ri+1]-y[ri])
   rrx <- rrr*(x[ri+1]-x[ri])+x[ri]
+  status <- rep(1,length(u))
+  status[is.na(rrx)] <- 0
   rrx[is.na(rrx)] <- mm
-  status <- rbinom(n,1,F1m)
-  dt <- data.frame(time=rrx,status=status)
+  dt <- data.frame(time=rrx,status=status,y=u)
+  if (!is.null(entry)) dt <- cbind(dt,entry)
   attr(dt,"F1") <- F1
   attr(dt,"Fmax") <- F1m
   return(dt)
 }# }}}
+
 
 #' @export
 subdist <- function(F1,times)
@@ -709,7 +868,7 @@ znames <- names(Z);
 model <- des$model
 
 if (class(cif)!="phreg") {
-if (model=="logistic2" | model=="logistic") ptt <- invsubdist(cumhazard,rr,type="logistic",...) else ptt <- invsubdist(cumhazard,rr,type="cloglog",...)
+if (model=="logistic2" | model=="logistic") ptt <- simsubdist(cumhazard,rr,type="logistic",...) else ptt <- simsubdist(cumhazard,rr,type="cloglog",...)
     ptt <- cbind(ptt,Z)
 } else {
 	ptt <- data.frame()
